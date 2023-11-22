f072bf9b24d26ea053edca4326903de3c82fdd35
Roy <92612927+PetarWho@users.noreply.github.com>
2023-11-22 16:41:02 +0200

    Completion of tasks/subtasks

--- Back/TaskDo/Controllers/SubtaskController.cs
+++ Back/TaskDo/Controllers/SubtaskController.cs
@@ -98,12 +98,12 @@ namespace TaskDo.Controllers
 
         public async Task<IActionResult> GetAllSubtasksForTask(Guid taskId)
         {
-            if (!await _context.Tasks.AnyAsync(x=>x.Id == taskId))
+            if (!await _context.Tasks.AnyAsync(x => x.Id == taskId))
             {
                 return NotFound("No such task");
             }
 
-            var subtasks = await _context.Subtasks.Where(x=>x.TaskId == taskId).ToListAsync();
+            var subtasks = await _context.Subtasks.Where(x => x.TaskId == taskId).ToListAsync();
             return Ok(subtasks);
         }
 
@@ -115,8 +115,8 @@ namespace TaskDo.Controllers
         [HttpGet("get_by_id")]
         public async Task<IActionResult> GetSubtaskById(Guid subtaskId)
         {
-            var subtask = await _context.Subtasks.Include(x=>x.Notes).Include(x=>x.Images)
-                             .Include(x=>x.Task)
+            var subtask = await _context.Subtasks.Include(x => x.Notes).Include(x => x.Images)
+                             .Include(x => x.Task)
                              .FirstOrDefaultAsync(x => x.Id == subtaskId);
             if (subtask == null)
             {
@@ -139,7 +139,7 @@ namespace TaskDo.Controllers
         [HttpPost("add_image")]
         public async Task<IActionResult> AddImageToSubtask(Guid subtaskId, string imagePath)
         {
-            var subtask = await _context.Subtasks.FirstOrDefaultAsync(x=>x.Id == subtaskId);
+            var subtask = await _context.Subtasks.Include(x => x.Images).Include(x=>x.Notes).FirstOrDefaultAsync(x => x.Id == subtaskId);
             if (subtask == null)
             {
                 return NotFound("Subtask not found");
@@ -151,6 +151,19 @@ namespace TaskDo.Controllers
             };
 
             await _context.AddAsync(image);
+            var task = await _context.Tasks.Include(x => x.Subtasks).FirstOrDefaultAsync(x => x.Id == subtask.TaskId);
+            try
+            {
+                if (task == null)
+                {
+                    throw new ArgumentException("Task associated with this subtask not found");
+                }
+                CheckCompletion(subtask, task);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
             await _context.SaveChangesAsync();
             return Ok();
         }
@@ -168,7 +181,7 @@ namespace TaskDo.Controllers
         [HttpPost("add_note")]
         public async Task<IActionResult> AddNoteToSubtask(Guid subtaskId, string noteText)
         {
-            var subtask = await _context.Subtasks.FirstOrDefaultAsync(x => x.Id == subtaskId);
+            var subtask = await _context.Subtasks.Include(x => x.Images).Include(x => x.Notes).FirstOrDefaultAsync(x => x.Id == subtaskId);
             if (subtask == null)
             {
                 return NotFound("Subtask not found");
@@ -180,8 +193,42 @@ namespace TaskDo.Controllers
             };
 
             await _context.AddAsync(note);
+            var task = await _context.Tasks.Include(x => x.Subtasks).FirstOrDefaultAsync(x => x.Id == subtask.TaskId);
+            try
+            {
+                if (task == null)
+                {
+                    throw new ArgumentException("Task associated with this subtask not found");
+                }
+                CheckCompletion(subtask, task);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
             await _context.SaveChangesAsync();
             return Ok();
+        }
+
+        #endregion
+
+        #region Check Subtask
+
+        private void CheckCompletion(Subtask subtask, Data.Entities.Task task)
+        {
+            if (subtask.Images.Count() >= subtask.RequiredPhotosCount && subtask.Notes.Count() >= subtask.RequiredNotesCount)
+            {
+                if (task != null && task.Status == Data.Entities.Enums.StatusEnum.Uncompleted)
+                {
+                    throw new ArgumentException("You cannot complete subtasks of Unfinished (Ended) Tasks.");
+                }
+
+                subtask.IsFinished = true;
+                if (task != null && task.Subtasks.All(x => x.IsFinished))
+                {
+                    task.Status = Data.Entities.Enums.StatusEnum.Completed;
+                }
+            }
         }
 
         #endregion
