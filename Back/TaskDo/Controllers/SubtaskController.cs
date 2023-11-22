0c2a361120ca42aed73ef80f0ebaa42e11ab6c71
Roy <92612927+PetarWho@users.noreply.github.com>
2023-11-22 15:06:08 +0200

    Added Controllers Documentation
    also moved JWT Decoder/Encoder to static class

--- Back/TaskDo/Controllers/SubtaskController.cs
+++ Back/TaskDo/Controllers/SubtaskController.cs
@@ -8,10 +8,15 @@ using TaskDo.Models.Subtask;
 
 namespace TaskDo.Controllers
 {
+    /// <summary>
+    /// Controller for managing Subtasks
+    /// </summary>
     [ApiController]
     [Route("api/subtasks")]
     public class SubtaskController : ControllerBase
     {
+        #region Injection
+
         private readonly ApplicationDbContext _context;
         private readonly JsonSerializerOptions _jsonOptions;
 
@@ -25,6 +30,16 @@ namespace TaskDo.Controllers
             };
         }
 
+        #endregion
+
+        #region Edit
+
+        /// <summary>
+        /// Action for editing subtasks
+        /// </summary>
+        /// <param name="id">Subtask ID</param>
+        /// <param name="updatedSubtask">The updated subtask</param>
+        /// <returns>200 if edit is successful or 404 if subtask not found</returns>
         [HttpPut("edit")]
         public IActionResult UpdateSubtask(Guid id, SubtaskModel updatedSubtask)
         {
@@ -45,6 +60,15 @@ namespace TaskDo.Controllers
             return Ok("Edit Successful");
         }
 
+        #endregion
+
+        #region Delete
+
+        /// <summary>
+        /// Action for deleting a subtask
+        /// </summary>
+        /// <param name="id">Subtask ID</param>
+        /// <returns>204 if deletion is successful or 404 if subtask not found</returns>
         [HttpDelete("delete")]
         public IActionResult DeleteSubtask(Guid id)
         {
@@ -61,17 +85,33 @@ namespace TaskDo.Controllers
             return NoContent();
         }
 
+        #endregion
+
+        #region Get Subtasks
+
+        /// <summary>
+        /// Get all subtask for given task
+        /// </summary>
+        /// <param name="taskId">Given task ID</param>
+        /// <returns>List of Subtasks or 404 if taskId is invalid</returns>
         [HttpGet("all")]
+
         public async Task<IActionResult> GetAllSubtasksForTask(Guid taskId)
         {
-            var subtasks = await _context.Subtasks.Where(x=>x.TaskId == taskId).ToListAsync();
-            if (subtasks == null)
+            if (!await _context.Tasks.AnyAsync(x=>x.Id == taskId))
             {
                 return NotFound("No such task");
             }
+
+            var subtasks = await _context.Subtasks.Where(x=>x.TaskId == taskId).ToListAsync();
             return Ok(subtasks);
         }
 
+        /// <summary>
+        /// Get subtask by ID
+        /// </summary>
+        /// <param name="subtaskId">Subtask ID</param>
+        /// <returns>Subtask if found or 404 if not found</returns>
         [HttpGet("get_by_id")]
         public async Task<IActionResult> GetSubtaskById(Guid subtaskId)
         {
@@ -86,6 +126,16 @@ namespace TaskDo.Controllers
             return Ok(serializedTask);
         }
 
+        #endregion
+
+        #region Subtask Image
+
+        /// <summary>
+        /// Add image to Subtask
+        /// </summary>
+        /// <param name="subtaskId">Subtask ID</param>
+        /// <param name="imagePath">Local path of the image</param>
+        /// <returns>200 for success or 404 if Subtask is not found</returns>
         [HttpPost("add_image")]
         public async Task<IActionResult> AddImageToSubtask(Guid subtaskId, string imagePath)
         {
@@ -105,6 +155,16 @@ namespace TaskDo.Controllers
             return Ok();
         }
 
+        #endregion
+
+        #region Subtask Note
+
+        /// <summary>
+        /// Add Note to Subtask
+        /// </summary>
+        /// <param name="subtaskId">Subtask ID</param>
+        /// <param name="noteText">Note as string</param>
+        /// <returns>200 for success or 404 if Subtask is not found</returns>
         [HttpPost("add_note")]
         public async Task<IActionResult> AddNoteToSubtask(Guid subtaskId, string noteText)
         {
@@ -123,5 +183,7 @@ namespace TaskDo.Controllers
             await _context.SaveChangesAsync();
             return Ok();
         }
+
+        #endregion
     }
 }
---
+++ Back/TaskDo/Utils/JwtUtils.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using TaskDo.Data;
+using TaskDo.Data.Entities;
+
+namespace TaskDo.Utils
+{
+    /// <summary>
+    /// Utility class for JSON Web Tokens
+    /// </summary>
+    public static class JwtUtils
+    {
+        /// <summary>
+        /// Decoder for JSON Web Tokens
+        /// </summary>
+
+        public static class JwtDecoder
+        {
+            /// <summary>
+            /// Decode the token back to readable JSON
+            /// </summary>
+            /// <param name="token">JWT to decode</param>
+            /// <returns>Decoded data as JSON</returns>
+            public static string GetTokenAsJson(string token)
+            {
+                string secret = "ASDFGHJKLQWERTYUIOPZXCVBNM1234567890";
+                var key = Encoding.ASCII.GetBytes(secret);
+                var handler = new JwtSecurityTokenHandler();
+                var validations = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+
+                var claimsPrincipal = handler.ValidateToken(token, validations, out var tokenSecure);
+
+                var claims = claimsPrincipal.Claims
+                    .Where(c => !string.IsNullOrEmpty(c.Type) && !string.IsNullOrEmpty(c.Value))
+                    .ToDictionary(c => GetKeyFromClaimType(c.Type), c => c.Value);
+
+                var tokenDataAsSimplifiedJson = JsonSerializer.Serialize(claims);
+
+                return tokenDataAsSimplifiedJson;
+            }
+            private static string GetKeyFromClaimType(string claimType)
+            {
+                int lastIndex = claimType.LastIndexOf('/');
+                return lastIndex >= 0 ? claimType.Substring(lastIndex + 1) : claimType;
+            }
+        }
+
+        /// <summary>
+        /// Encoder for JSON Web Tokens  
+        /// </summary>
+        public static class JwtEncoder
+        {
+            /// <summary>
+            /// Generates JSON Web Token using User ID and Role
+            /// </summary>
+            /// <param name="user">Given user</param>
+            /// <param name="role">Given user's role</param>
+            /// <param name="_context">DbContext for saving the created token to database</param>
+            /// <returns>Generated token as string</returns>
+            public static string GenerateJwtToken(User user, string role, ApplicationDbContext _context)
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("ASDFGHJKLQWERTYUIOPZXCVBNM1234567890");
+                var expiryDate = DateTime.UtcNow.AddHours(24);
+
+                var claims = new List<Claim>
+                {
+                    new Claim("id", user.Id.ToString()),
+                    new Claim("role", role)
+                };
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = expiryDate,
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenStr = tokenHandler.WriteToken(token);
+                _context.JsonWebTokens.Add(new JsonWebToken()
+                {
+                    Token = tokenStr,
+                    UserId = user.Id,
+                    ExpiryDate = expiryDate
+                });
+                _context.SaveChanges();
+                return tokenStr;
+            }
+        }
+    }
+}
